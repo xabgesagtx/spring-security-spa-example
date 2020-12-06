425a35c9186c42824631f81eb004dceabe983a70
xabgesagtx <xabgesagtx@riseup.net>
2020-12-06 12:49:59 +0100

    User predefined http status entrypoint

--- spring-gateway-security-spa/src/main/java/com/github/xabgesagtx/springgatewaysecurityspa/CustomSecurityConfig.java
+++ spring-gateway-security-spa/src/main/java/com/github/xabgesagtx/springgatewaysecurityspa/CustomSecurityConfig.java
@@ -1,8 +1,8 @@
 package com.github.xabgesagtx.springgatewaysecurityspa;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
@@ -11,21 +11,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 
 @Configuration
 @EnableWebFluxSecurity
-@RequiredArgsConstructor
 public class CustomSecurityConfig {
-
-    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityWebFilterChain securitygWebFilterChain(
             ServerHttpSecurity http) {
         return http
                 .httpBasic()
-                    .authenticationEntryPoint(authenticationEntryPoint)
+                    .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
                     .securityContextRepository(new WebSessionServerSecurityContextRepository())
                     .and()
                 .csrf()
